8b16996aad91d4a6794cb8fb7b6845c522fcf115
Delacerna08 <102667333+Delacerna08@users.noreply.github.com>
2023-08-23 22:30:25 +0800

    Add files via upload

---
+++ loginform.js
@@ -0,0 +1,60 @@
+import React, {useState} from "react";
+import "./loginform.css"
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('https://hi-clist-be.vercel.app//api/user/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      });
+
+      if (response.ok) {
+        // Successful login logic (e.g., redirect to dashboard)
+        console.log('Login successful');
+      } else {
+        const errorData = await response.json();
+        setError(errorData.message);
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <button type="submit">Login</button>
+      </form>
+      {error && <p className="error">{error}</p>}
+    </div>
+  );
+};
+
+export default Login;
